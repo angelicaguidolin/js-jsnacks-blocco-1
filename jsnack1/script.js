3e82fb84289418030ec908b71bfd17fd4905e230
angelica <guidolinangelica66@gmail.com>
2024-12-03 11:41:25 +0100

    finish project

---
+++ jsnack1/script.js
@@ -0,0 +1,21 @@
+//L’utente inserisce due numeri in successione, con due prompt.
+//Il software stampa il maggiore
+
+const numb1 = prompt("inserisci un numero")
+const numb2 =prompt("inserisci un numero")
+if( numb1 > numb2){
+ alert("numb1 è maggiore")
+
+} else if(numb1 < numb2){
+    alert("numb2 è maggiore")
+}else (numb1 == numb2) {
+    alert("i numeri sono uguali")
+
+ 
+
+}
+    
+
+
+
+
