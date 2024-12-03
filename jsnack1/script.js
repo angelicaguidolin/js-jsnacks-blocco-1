fae4a3337cb4f1dff7d91ca5a7a6242762bfbfce
angelica <guidolinangelica66@gmail.com>
2024-12-03 11:46:45 +0100

    feat fix alert

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,17 +4,11 @@
 const numb1 = prompt("inserisci un numero")
 const numb2 =prompt("inserisci un numero")
 if( numb1 > numb2){
- alert("numb1 è maggiore")
+ Number(alert(numb1))
 
 } else if(numb1 < numb2){
-    alert("numb2 è maggiore")
-}else (numb1 == numb2) {
-    alert("i numeri sono uguali")
-
- 
-
-}
-    
+    Number(alert(numb2))
+} 
 
 
 
